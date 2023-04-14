8512b2462c7cd97f53248c446cc8f2ee97cc8cfa
Akira Otaka <akira.otaka@dgshape.com>
2023-04-14 12:43:12 +0900

    initial commit (remake project)

---
+++ AspDotNet.Skelton.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspDotNet.Skelton.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
